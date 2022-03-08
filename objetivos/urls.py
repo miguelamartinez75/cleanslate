78a77e4fb396dcb3a006e1015681c81e7c79a271
Miguel Angel Martinez <miguelamartinez@gmail.com>
2022-03-08 10:23:33 -0300

    PATCH (Update) y DELETE de Estructura

--- objetivos/urls.py
+++ objetivos/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('est/', views.getEstructura, name='estructura'),
-    path('setest/', views.setEstructura, name='set-estructura'),
+    path('est/', views.getEstructura),
+    path('est/<int:id>', views.getEstructura),
+    path('setest/', views.setEstructura),
+    path('setest/<int:id>', views.patchEstructura),
+    path('delest/<int:id>', views.delEstructura),
     path('subest/', views.getHijosDe, name='desendientes'),
-    path('objs/', views.getObjetivo, name='objetivo'),
+    path('objs/', views.getObjetivo),
 ]
--- objetivos/views.py
+++ objetivos/views.py
@@ -1,18 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Estructura, Objetivo
 from .serializers import EstructuraSerializer, ObjetivoSerializer
 
 
 @api_view(["GET"])
-def getEstructura(request):
+def getEstructura(request, id=None):
+    if id:
+        item = Estructura.objects.get(id=id)
+        serializer = EstructuraSerializer(item)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
     miembros = Estructura.objects.all()
     serializer = EstructuraSerializer(miembros, many=True)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(['PUT'])
 def setEstructura(request):
     serializer = EstructuraSerializer(data=request.data)
     if serializer.is_valid():
@@ -20,6 +26,23 @@ def setEstructura(request):
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
     else:
         return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PATCH'])
+def patchEstructura(request, id=None):
+    item = Estructura.objects.get(id=id)
+    serializer = EstructuraSerializer(item, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"status": "success", "data": serializer.data})
+    else:
+        return Response({"status": "error", "data": serializer.errors})
+
+@api_view(['DELETE'])
+def delEstructura(request, id=None):
+    item = get_object_or_404(Estructura, id=id)
+    item.delete()
+    return Response({"status": "success", "data": "Item Deleted"})
 
 
 @api_view(['GET'])
